eb788a0c001dcf52b45c9fe6710055198b89ed86
Rafif Suta Pratama <rafiftopup@gmail.com>
2025-07-09 04:41:16 +0700

    Loginform

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/loginform', function () {
+    return view('loginform.login'); 
+});
+
+Route::get('/register', function() {
+    return view('auth.register');
+})->name('register');
