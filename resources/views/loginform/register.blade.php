12af9e18116d918d51fed9d9774f4a1e5a3c3e4e
Rafif Suta Pratama <rafiftopup@gmail.com>
2025-07-17 20:19:17 +0700

    perbaikan kolom input form register  agar terlihat

--- resources/views/loginform/register.blade.php
+++ resources/views/loginform/register.blade.php
@@ -21,21 +21,21 @@
                             @csrf
                             <div class="mb-3">
                                 <label>Nama</label>
-                                <input type="text" name="name" class="form-control" value="{{ old('name') }}"
+                                <input type="text" name="name" class="form-control shadow-sm border border rounded-4 py-2 px-2" value="{{ old('name') }}"
                                     required>
                             </div>
                             <div class="mb-3">
                                 <label>Email</label>
-                                <input type="email" name="email" class="form-control" value="{{ old('email') }}"
+                                <input type="email" name="email" class="form-control shadow-sm border border rounded-4 py-2 px-2" value="{{ old('email') }}"
                                     required>
                             </div>
                             <div class="mb-3">
                                 <label>Password</label>
-                                <input type="password" name="password" class="form-control" required>
+                                <input type="password" name="password" class="form-control shadow-sm border border rounded-4 py-2 px-2" required>
                             </div>
                             <div class="mb-3">
                                 <label>Konfirmasi Password</label>
-                                <input type="password" name="password_confirmation" class="form-control" required>
+                                <input type="password" name="password_confirmation" class="form-control shadow-sm border border rounded-4 py-2 px-2" required>
                             </div>
                             <button type="submit" class="btn btn-primary w-100">Register</button>
                         </form>
